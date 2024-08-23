cb6acb0a01dd207b3dce7ec2e0c60d122c2400fc
gui117 <48198890+gui117@users.noreply.github.com>
2024-08-23 09:34:27 +0800

    chore: migrate to Nuxt 4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  future: {
+    compatibilityVersion: 4,
+  },
+
   devtools: { enabled: true },
+
   modules: [
     '@unocss/nuxt',
   ],
+
   css: [
     '@unocss/reset/normalize.css',
     '~/global.css',
   ],
+
+  compatibilityDate: '2024-08-23',
 })
